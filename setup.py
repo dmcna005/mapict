521c60b6c22dc162c9ea97a1db8347da97cdfefa
Dwayne McNab <dwayneexec@gmail.com>
2018-05-04 12:33:22 -0400

    new update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='A command line tool that uses restful api call to mongodb',
     url='https://github.com/dmcna005/opsmanagerapi',
     author='Dwayne McNab',
-    author_email='dwayne.mcnab@equifax.com',
+    author_email='dwayneexec@gmail.com',
     license='GNU',
     packages=['opsmanagerapi']
 
